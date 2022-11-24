b78befd466ba2bb1dffde9dce5ba865bd9d61fd6
sagorsikder <mdsagorsikder97@gmail.com>
2022-11-25 01:31:48 +0600

    login upate

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -5,20 +5,20 @@ const About = () => {
     return (
         <div className="hero my-12">
   <div className="hero-content flex flex-col">
-   <div className='relative h-full mb-12 w-1/2'>
+   <div className='relative h-full  mb-12 w-1/2'>
 
-     <img src='https://th.bing.com/th/id/R.e0eb4a9c301a4839479331db2e5318c6?rik=%2bhGt%2f8JuvwAhMw&pid=ImgRaw&r=0' alt='good' className="w-4/5 h-full max-w-sm rounded-lg shadow-2xl" />
-     <img src='https://th.bing.com/th/id/OIP.PvVjgQdea-ruiFkLkbtk1AHaE8?pid=ImgDet&w=800&h=534&rs=1' alt='good' className="max-w-sm absolute right-12  top-1/3 border-8 w-1/2 rounded-lg shadow-2xl" />
+     <img src='https://th.bing.com/th/id/R.a6598f3f4307a62cbb7f22832faa660a?rik=BrD7dGMpp8q85g&pid=ImgRaw&r=0' alt='good' className="w-4/5 h-full max-w-sm rounded-lg shadow-2xl" />
+    
 
      </div>
     <div className='w-1/2'>
-        <h4 className='text-orange-600 font-bold text-xl mb-2'>About Us</h4>
-      <h1 className="text-5xl font-bold">I am qualified <br />
-       & of experience <br /> in this field</h1>
-      <p className="py-6">Photography is my emotion,hobby,passion,profession.Always i was try to best performance,and i have services with low cost. </p>
+        <h4 className='text-blue-600 font-bold text-xl mb-2'>About Us</h4>
+      <h1 className="text-5xl font-bold">Always stock available<br />
+       Best bike collection <br /> On-time delivery</h1>
+      <p className="py-6">The Second hand bike price list is based on the make, model, variant, specifications, and year. The pre-owned bikes are from popular brands like Hero MotoCorp, TVS, Bajaj, etc </p>
 
-      <p className="py-6">Wedding,birthday party,consert etc,all event handle with my Gorgeous camera and my team.Maximum client satisfied with me. </p>
-      <button className="btn btn-warning rounded text-white"> More Info</button>
+      <p className="py-6">Hero Motor, TVS, Bajaj, Yamaha etc,all brand bike is available for sell and buy.Maximum client satisfied with this website. </p>
+      <button className="btn btn-secondary rounded "> More Information</button>
     </div>
   </div>
 </div>
