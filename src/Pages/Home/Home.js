c008ba077d3042d08aea30d60b043731f383d5c7
sagorsikder <mdsagorsikder97@gmail.com>
2022-11-27 01:21:01 +0600

    modal update successfully

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import useTitle from '../../Hooks/useTitle';
 import About from './About/About';
 import Advertisement from './advertisement/Advertisement';
 import Banner from './Banner/Banner';
 import Category from './Category/Category';
 
 const Home = () => {
+    useTitle('Old Is Gold')
 
     return (
         <div>
--- src/Utilities/Modal.js
+++ src/Utilities/Modal.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/UserContext';
 
 const Modal = ({product}) => {
 
-    const {img,name,location,orginalPrice,resalePrice,used,seller} = product;
-    console.log(img,name,location,orginalPrice,resalePrice,used,seller)
+  const {user} = useContext(AuthContext)
+  const {email} = user;
+  console.log(email)
+    const {img,name,orginalPrice} = product;
+
+    const handleSubmit =(event)=>{
+
+      event.preventDefault()
+      const form = event.target;
+      const name = form.name.value;
+      const email = form.email.value;
+      const product = form.product.value;
+      const price = form.price.value;
+      const location = form.location.value;
+      const phone = form.phone.value;
+
+      const order = {
+        name,
+        email,
+        product,
+        price,
+        location,
+        phone
+      }
+
+      console.log(order)
+      alert('order successfully')
+
+    }
+    
 
 
     return (
@@ -14,10 +43,57 @@ const Modal = ({product}) => {
   <div className="modal-box">
     <img src={img} alt="" />
     <h3 className="font-bold text-lg">{name}</h3>
-    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
-    <div className="modal-action">
-      <label htmlFor="booking-modal" className="btn">Yay!</label>
+    <p className="py-4">Please fill up this form for ensure your order!!</p>
+
+    <form onSubmit={handleSubmit}>
+    <div className="form-control w-full ">
+  <label className="label">
+    <span className="label-text">Name</span>
+  </label>
+  <input type="text" name='name' placeholder="Name " className="input input-bordered w-full " />
+ 
+</div>
+    <div className="form-control w-full ">
+  <label className="label">
+    <span className="label-text">Email</span>
+  </label>
+  <input type="text" name='email' defaultValue={email} disabled className="input input-bordered w-full " />
+ 
+</div>
+    <div className="form-control w-full ">
+  <label className="label">
+    <span className="label-text">Product</span>
+  </label>
+  <input type="text" name='product' defaultValue={name} disabled className="input input-bordered w-full " />
+ 
+</div>
+    <div className="form-control w-full ">
+  <label className="label">
+    <span className="label-text">Price</span>
+  </label>
+  <input type="text" name='price' defaultValue={orginalPrice} className="input input-bordered w-full " />
+ 
+</div>
+    <div className="form-control w-full ">
+  <label className="label">
+    <span className="label-text">Phone</span>
+  </label>
+  <input type="text" name='phone' placeholder="Type your phone no " className="input input-bordered w-full " />
+ 
+</div>
+    <div className="form-control w-full ">
+  <label className="label">
+    <span className="label-text">Meeting location</span>
+  </label>
+  <input type="text" name='location' placeholder="Type your location " className="input input-bordered w-full " />
+ 
+</div>
+
+<div className="modal-action">
+<input  htmlFor="booking-modal" className="btn" type="submit" value="Submit" />
     </div>
+    </form>
+    
   </div>
 </div>
         </div>
