70e9c8c29574c50cba74e48cf2b3e8c3244d73fa
sagorsikder <mdsagorsikder97@gmail.com>
2022-12-08 16:24:39 +0600

    seller delete part update successfully

--- src/Pages/MySellers/MySellers.js
+++ src/Pages/MySellers/MySellers.js
@@ -2,8 +2,24 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const MySellers = () => {
+  
     const loadUsers = useLoaderData()
     const users = loadUsers.filter(user=>user?.role !== 'admin' && user?.role !== 'Buyer')
+    
+    const handleDelete =(email)=>{
+      
+
+
+          fetch(`https://old-is-gold-server.vercel.app/seller?email=${email}`)
+         
+   
+       
+        }
+    
+
+     
+      
+    
     return (
         <div>
             <h1 className='text-3xl font-semibold my-7'>All Seller</h1>
@@ -22,12 +38,12 @@ const MySellers = () => {
   
    {
 
-    users.map((user,i)=> <tr>
+   users && users.map((user,i)=> <tr>
         <th>{i+1}</th>
         <td>{user.name}</td>
         <td>{user.email}</td>
        
-        <td><button className='btn btn-warning'>delete</button></td>
+        <td><button onClick={()=>handleDelete(user.email)} className='btn btn-warning'>delete</button></td>
       </tr>)
    }
     </tbody>
